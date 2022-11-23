5da753a22a4bde1e85b66318c63477ee42c76d15
AnthoninKADI <117898338+AnthoninKADI@users.noreply.github.com>
2022-11-23 15:11:53 +0100

    Chest with 3 torch script update
    very cool update 1.0

--- Assets/Scripts/Marie/Items/Interactive/WallTorch.cs
+++ Assets/Scripts/Marie/Items/Interactive/WallTorch.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class WallTorch : Interactive
 {
+
+    public static int LitTorchesCount = 0;
+    public GameObject Chest;
     public override void OnInteraction()
     {
         //If I want to do the base OnInteraction anyway first
@@ -11,5 +14,10 @@ public class WallTorch : Interactive
         //Activate light and fire
         transform.GetChild(0).gameObject.SetActive(true);
         transform.GetChild(1).gameObject.SetActive(true);
+        LitTorchesCount++;
+        if(LitTorchesCount == 3)
+        {
+           Chest.SetActive(true);
+        }
     }
 }
